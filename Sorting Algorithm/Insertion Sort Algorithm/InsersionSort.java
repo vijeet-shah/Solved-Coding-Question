b659776ed0d9d3592e0e0b46bca02beae7dea9eb
vijeet-shah <93502358+vijeet-shah@users.noreply.github.com>
2023-04-25 20:42:24 +0530

    Add files via upload

---
+++ Sorting Algorithm/Insertion Sort Algorithm/InsersionSort.java
@@ -0,0 +1,19 @@
+class InsersionSort {
+    public static void main(String[] args) {
+        int[] arr = { 32, 12, 47, 19, 7, 25, 3 };
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
